73f2ba6faa37ab28ac2af0ca01d933d4d011e972
PC16\t.davydenkova <t.davydenkova@peppers-studio.ru>
2018-11-14 16:30:05 +0300

    - переключение языка для меню

--- app/components/main.js
+++ app/components/main.js
@@ -2,18 +2,24 @@ $(function(){
 
 	$.getJSON( "assets/data.json", function(data) {
 	  // console.log( "success", data );
-		$(window).trigger("main:ready", data);
+		$(window)
+      .trigger("main:ready", data)
+      .trigger('language-changed')
+    ;
+    
 	}).fail(function(e,error_message) {
     	console.log( "error", error_message );
   	});
 
-  $(window).on('change-page', function(event, content_id){
-    $('title').text(content_id);
-    $(window).trigger("page-changed", content_id );
-  });
-	$(window).on('change-language', function(event, language){
-    console.log(language);
-  });
+  $(window)
+    .on('change-page', function(event, content_id){
+      $('title').text(content_id);
+      $(window).trigger("page-changed", content_id );
+    })
+    .on('change-language', function(event, language){
+      $(window).trigger("language-changed", language );
+    })
+  ;
 
 });
 
--- app/components/menu-inner/menu-inner.js
+++ app/components/menu-inner/menu-inner.js
@@ -1,5 +1,9 @@
 $(window).on("main:ready", function(event, data){
 	
+	var def_lang = data.default_lang;
+
+	console.log("def_lang =", def_lang);
+
 	var $element = $('.menu-inner');
 	if( !$element.length ) return;
 
@@ -7,21 +11,30 @@ $(window).on("main:ready", function(event, data){
 	// var $item = $('.manu-inner__item', $element );
 
 	console.log('menu', data.menu);
+	
+	var $DOM_menu_items = [];
 
 	// найти меню в объекте
 	// перебрать все элементы этого массива
-
 	for (var i = data.menu.length - 1 ; i>=0; i--){
+
 		var item = data.menu[i];
-		// console.log(i, item);
+		console.log( i, item );
 
 		// каждому элементу создать DOM-элемент списка меню на основе данных
 		if (item.type == 'line') {
 			$list.prepend( $('<li class="menu-inner__item menu-inner__item-line">'));	
 		} else {
-			$list.prepend($('<li class="menu-inner__item"><a data-content-id="'+ item.content_id +'" href="'+ item.href +'">'+ item.title +'</a></li>'));
+			var $menu_item =  $( '<li class="menu-inner__item"><a data-content-id="'+ item.content_id +'" href="'+ item.href +'"></a></li>' )
+				.data( 'translate', item.title )
+				.prependTo($list)
+			;
+			$DOM_menu_items.unshift( $menu_item );
 		}
 	}
+
+	// console.log('список', $DOM_menu_items);
+
 
 	//проверить есить ли отклик на элементе меню
 	//взять название и передать в название новой страницы
@@ -35,11 +48,22 @@ $(window).on("main:ready", function(event, data){
 
 	});
 
-	$(window).on('page-changed', function (event, content_id) {
-		// console.log('menu cought: page chanded', content_id );
-		$(':not([data-content-id="'+ content_id +'"])').removeClass('menu-inner__item_active');
-		$('[data-content-id="'+ content_id +'"]').addClass('menu-inner__item_active');
-	});
+	$(window)
+		.on('page-changed', function (event, content_id) {
+			// console.log('menu cought: page chanded', content_id );
+			
+			$(':not([data-content-id="'+ content_id +'"])').removeClass('menu-inner__item_active');
+			$('[data-content-id="'+ content_id +'"]').addClass('menu-inner__item_active');
 
+		})
+		.on('language-changed', function(event, language){
+			language = language || def_lang;
+			$DOM_menu_items.forEach(function($e,i){
+				// console.log(i, '>>', $e.data('translate') );
+				if (!$e.data( 'translate' )[language] == '') $('a', $e ).html( $e.data( 'translate' )[language] );
+			});
+
+		})
+	;
 
 });
