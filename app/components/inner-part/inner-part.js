0631fe5f021d3dc242a3a5cac4ad5ad7859455f1
PC16\t.davydenkova <t.davydenkova@peppers-studio.ru>
2018-12-05 16:02:37 +0300

    - анимация при смене контента

--- app/components/inner-part/inner-part.js
+++ app/components/inner-part/inner-part.js
@@ -10,6 +10,7 @@ $(window).on("main:ready", function(event, data){
 	var page = data.default_page;
 
 
+
 	var template = Handlebars.compile( $('#template-main-content').html());
 
 	console.log("template", template);
@@ -17,36 +18,48 @@ $(window).on("main:ready", function(event, data){
 
 	$(window)
 		.on('page-changed', function (event, content_id) {
-			// changeContent( content_id );
-			// console.log('!!!')
 
 			page = content_id;
-			qwerty(lang, content_id)
+			// changeContent(lang, content_id);
+			// fadeOut(changeContent);
+			fadeOut( function(){
+				changeContent(lang, content_id);
+			});
 		})		
 		.on('language-changed', function(event, language){
 			lang = language;
-			qwerty(language, page)
+			fadeOut( function(){
+				changeContent(language, page)
+			} );
+			// changeContent(language, page);
 		})
 	;
 
-
-	// function changeContent( content_id ){
-		// var obj_content = data.content[content_id];
+	/*
+	function changeContent( content_id ){
+		var obj_content = data.content[content_id];
 		// console.log("obj_content: ", obj_content );
-		// var html = template( obj_content );
+		var html = template( obj_content );
 		// console.log("html: ", $('#template-main-content').html(), html );
-		// $element.empty().html( html );
-	// }
+		$element.empty().html( html );
+	}
+	*/
+	function fadeOut( callback ) {
+		$element.fadeOut( function (){
+			if ( callback ) callback();
+		});
+	}
 
-	// function changeLanguage(language){
-	// }
 
-	function qwerty(language, content_id) {
+	function changeContent( language, content_id ) {
+
+		// $element.fadeOut();
 		var obj_content = data.content[content_id];
 		// console.log("obj_content: ", obj_content );
 		var html = template( ( { image: obj_content.image, title: obj_content.title[language], text:obj_content.text[language] } ) );
 		// var html = template(obj_content);
 		// console.log("html: ", $('#template-main-content').html(), html );
 		$element.empty().html( html );
+		$element.fadeIn();
 	}
 });
